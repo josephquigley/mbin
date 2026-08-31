d81970238499b1ac8912762e6302d95914773cee
Joseph Quigley <jquigley@digitallyuseful.com>
2026-08-31 11:27:06 -0400

    TEST: cover webfinger resolution of magazine handles and the random exclusion

--- tests/Functional/Controller/WebfingerControllerTest.php
+++ tests/Functional/Controller/WebfingerControllerTest.php
@@ -40,4 +40,48 @@ class WebfingerControllerTest extends WebTestCase
         self::assertTrue($jsonContent['manuallyApprovesFollowers']);
         self::assertNotEmpty($jsonContent['publicKey']);
     }
+
+    public function testMagazineActor(): void
+    {
+        $magazine = $this->getMagazineByName('acme');
+        $domain = $this->settingsManager->get('KBIN_DOMAIN');
+        $resource = "acct:{$magazine->name}@$domain";
+        $this->client->request('GET', "https://$domain/.well-known/webfinger?resource=".urlencode($resource));
+
+        self::assertResponseIsSuccessful();
+        $jsonContent = self::getJsonResponse($this->client);
+
+        self::assertArrayHasKey('subject', $jsonContent);
+        self::assertEquals($resource, $jsonContent['subject']);
+        self::assertArrayHasKey('links', $jsonContent);
+
+        $selfLinks = array_values(array_filter($jsonContent['links'], fn ($link) => 'self' === $link['rel']));
+        self::assertCount(1, $selfLinks);
+        self::assertEquals('application/activity+json', $selfLinks[0]['type']);
+
+        $this->client->request('GET', $selfLinks[0]['href'], [], [], [
+            'HTTP_ACCEPT' => 'application/activity+json',
+        ]);
+
+        self::assertResponseIsSuccessful();
+        $jsonContent = self::getJsonResponse($this->client);
+        self::assertEquals('Group', $jsonContent['type']);
+        self::assertEquals($magazine->name, $jsonContent['preferredUsername']);
+        self::assertEquals($selfLinks[0]['href'], $jsonContent['id']);
+    }
+
+    /**
+     * The magazine called "random" is the local catch-all for content that belongs to no other
+     * magazine. It is deliberately kept out of federation entirely (see issue #444), so it must
+     * not be discoverable by handle either.
+     */
+    public function testRandomMagazineIsNotDiscoverable(): void
+    {
+        $this->getMagazineByName('random');
+        $domain = $this->settingsManager->get('KBIN_DOMAIN');
+        $resource = "acct:random@$domain";
+        $this->client->request('GET', "https://$domain/.well-known/webfinger?resource=".urlencode($resource));
+
+        self::assertResponseStatusCodeSame(404);
+    }
 }
