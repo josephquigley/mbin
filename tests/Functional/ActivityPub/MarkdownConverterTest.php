da73a32c0cedb0cb2f56a6d64f05c693f00fdc09
Joseph Quigley <jquigley@digitallyuseful.com>
2026-09-04 01:30:28 -0400

    TEST: cover hashtag links in ActivityPub HTML to markdown conversion

--- tests/Functional/ActivityPub/MarkdownConverterTest.php
+++ tests/Functional/ActivityPub/MarkdownConverterTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Functional\ActivityPub;
 
 use App\Entity\User;
+use App\Service\TagExtractor;
 use PHPUnit\Framework\Attributes\DataProvider;
 
 use function PHPUnit\Framework\assertEquals;
@@ -101,6 +102,64 @@ class MarkdownConverterTest extends ActivityPubFunctionalTestCase
                 ],
                 'expectedMentions' => ['@SomeAccount@mastodon.tld'],
                 'name' => 'Mastodon account mention',
+            ],
+        ];
+    }
+
+    #[DataProvider('htmlHashtagsProvider')]
+    public function testHashtags(string $html, array $apTags, ?array $expectedTags, string $name): void
+    {
+        $converted = $this->apMarkdownConverter->convert($html, $apTags);
+        $tags = (new TagExtractor())->extract($converted);
+        assertEquals($expectedTags, $tags, message: "Hashtag test '$name'");
+    }
+
+    public static function htmlHashtagsProvider(): array
+    {
+        return [
+            [
+                'html' => '<p>hello</p>',
+                'apTags' => [],
+                'expectedTags' => null,
+                'name' => 'No hashtag at all',
+            ],
+            [
+                'html' => '<p>hello #foo</p>',
+                'apTags' => [],
+                'expectedTags' => ['foo'],
+                'name' => 'Plain text hashtag, the control case',
+            ],
+            [
+                'html' => '<a href="https://writefreely.tld/tag:foo">#foo</a>',
+                'apTags' => [
+                    ['type' => 'Hashtag', 'href' => 'https://writefreely.tld/tag:foo', 'name' => '#foo'],
+                ],
+                'expectedTags' => ['foo'],
+                'name' => 'WriteFreely hashtag link',
+            ],
+            [
+                'html' => '<p>a post about <a href="https://writefreely.tld/tag:foo">#foo</a> and more</p>',
+                'apTags' => [
+                    ['type' => 'Hashtag', 'href' => 'https://writefreely.tld/tag:foo', 'name' => '#foo'],
+                ],
+                'expectedTags' => ['foo'],
+                'name' => 'WriteFreely hashtag link surrounded by prose',
+            ],
+            [
+                'html' => '<p><a href="https://mastodon.tld/tags/foo" class="mention hashtag" rel="tag">#<span>foo</span></a></p>',
+                'apTags' => [
+                    ['type' => 'Hashtag', 'href' => 'https://mastodon.tld/tags/foo', 'name' => '#foo'],
+                ],
+                'expectedTags' => ['foo'],
+                'name' => 'Mastodon hashtag link',
+            ],
+            [
+                'html' => '<p>nothing inline here</p>',
+                'apTags' => [
+                    ['type' => 'Hashtag', 'href' => 'https://writefreely.tld/tag:foo', 'name' => '#foo'],
+                ],
+                'expectedTags' => null,
+                'name' => 'Hashtag only in the tag array is not in the converted content',
             ],
         ];
     }
